b966e6d16612810f4167fd01adf1594c2af0b823
Stefan Sauer <ensonic@google.com>
2014-10-21 17:07:10 +0200

    Move autodetect handling to the library.
    This is consistent with how we expand the settings. Also add comments to
    separate code that deals with settings and registration parameters.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ exports.getLibraryConfig = function () {
   return JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
 };
 
+// user settings
 exports.getDefaultsPath = function () {
   var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
   return path.resolve(homeDir, '.flowhub.json');
@@ -29,16 +30,23 @@ exports.saveDefaults = function (values) {
   var defaultsPath = exports.getDefaultsPath();
   fs.writeFileSync(defaultsPath, JSON.stringify(values, null, 2), 'utf-8');
 };
+
+// flowhub registration
 exports.getStoredPath = function () {
   var root = process.env.PROJECT_HOME || process.cwd();
   return path.resolve(root, 'flowhub.json');
 };
 exports.getStored = function () {
+  var stored = {};
   var storedPath = exports.getStoredPath();
   if (fs.existsSync(storedPath)) {
-    return JSON.parse(fs.readFileSync(storedPath));
+    stored = JSON.parse(fs.readFileSync(storedPath));
+
+    if (stored.host === 'autodetect') {
+      stored.host = exports.discoverHost();
+    }    
   }
-  return {};
+  return stored;
 };
 exports.saveStored = function (values) {
   var storedPath = exports.getStoredPath();
