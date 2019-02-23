632a19c874d588ece9440f1ab7c515be3a9c47e0
Henri Bergius <henri.bergius@iki.fi>
2019-02-23 17:43:53 +0100

    Get rid of Grunt

---
+++ spec/protocol.js
@@ -0,0 +1,37 @@
+const { spawn, exec } = require('child_process');
+const path = require('path');
+
+describe('FBP Protocol Compatibility', () => {
+  const prog = path.resolve(__dirname, '../bin/noflo-nodejs');
+  const tester = path.resolve(__dirname, '../node_modules/.bin/fbp-test --colors');
+  const runtimeSecret = process.env.FBP_PROTOCOL_SECRET || 'noflo-nodejs';
+  let progProcess;
+  before('start runtime', (done) => {
+    progProcess = spawn(prog, [
+      '--host=localhost',
+      '--port=8080',
+      '--open=false',
+      `--secret=${runtimeSecret}`,
+    ]);
+    done();
+  });
+  after('stop runtime', (done) => {
+    if (!progProcess) {
+      done();
+      return;
+    }
+    process.kill(progProcess.pid);
+    done();
+  });
+  it('should pass the test suite', (done) => {
+    exec(tester, {
+      env: {
+        FBP_PROTOCOL_SECRET: runtimeSecret,
+      },
+    }, (err, stdout, stderr) => {
+      console.log(stdout);
+      console.error(stderr);
+      done(err);
+    });
+  }).timeout(20000);
+});
