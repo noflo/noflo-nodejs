c76cd3dd17f18b72fe7aa17055fa643be6cc3ea9
Henri Bergius <henri.bergius@iki.fi>
2020-10-01 15:01:23 +0200

    Initial WebRTC protocol tests

--- spec/cli.js
+++ spec/cli.js
@@ -1,6 +1,7 @@
 const { exec, spawn } = require('child_process');
 const { expect } = require('chai');
 const { promisify } = require('util');
+const { v4: uuid } = require('uuid');
 const path = require('path');
 const fs = require('fs');
 const fbpHealthCheck = require('fbp-protocol-healthcheck');
@@ -300,6 +301,49 @@ cases:
           const graphJson = JSON.parse(contents);
           expect(graphJson).to.eql(JSON.parse(JSON.stringify(graphInstance.toJSON())));
         }));
+    });
+  });
+  describe('--protocol=webrtc', () => {
+    const baseDir = path.resolve(__dirname, './fixtures/graph-as-component');
+    const graph = path.resolve(baseDir, './graphs/helloin.fbp');
+    let runtimeProcess;
+    let runtimeClient;
+    const runtimeId = uuid();
+    before('start runtime', (done) => {
+      runtimeProcess = spawn(prog, [
+        '--open=false',
+        `--id=${runtimeId}`,
+        `--secret=${runtimeSecret}`,
+        `--base-dir=${baseDir}`,
+        `--graph=${graph}`,
+      ]);
+      runtimeProcess.stdout.pipe(process.stdout);
+      runtimeProcess.stderr.pipe(process.stderr);
+      healthCheck(`webrtc://#${runtimeId}`, done);
+    });
+    it('should be possible to connect', () => fbpClient({
+      address: runtimeId,
+      protocol: 'websocket',
+      secret: runtimeSecret,
+    })
+      .then((c) => {
+        runtimeClient = c;
+        return c.connect();
+      }));
+    it('should have marked the graph as the main', () => {
+      expect(runtimeClient.definition.graph).to.equal('graph-as-component/HelloIn');
+    });
+    it('should be possible to get graph sources', () => runtimeClient
+      .protocol.component.getsource({
+        name: 'graph-as-component/HelloIn',
+      }));
+    after('stop runtime', (done) => {
+      if (!runtimeProcess) {
+        done();
+        return;
+      }
+      process.kill(runtimeProcess.pid);
+      done();
     });
   });
 });
